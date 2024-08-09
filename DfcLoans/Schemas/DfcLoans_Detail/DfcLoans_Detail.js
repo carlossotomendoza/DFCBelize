c6dbbb6594d808ea89fcadbf0656d65a8874a454
dougmarino <33579074+dougmarino@users.noreply.github.com>
2024-08-09 13:16:52 -0400

    20240809-1316 Rebase

---
+++ DfcLoans/Schemas/DfcLoans_Detail/DfcLoans_Detail.js
@@ -0,0 +1,8 @@
+define('DfcLoans_Detail', [], function () {
+	return {
+		entitySchemaName: 'DfcLoans',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
