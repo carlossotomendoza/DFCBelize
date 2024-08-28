5c85a3616419ff3996c5bb2a85a1d453957dab14
carlossotomendoza <carlos.soto@stark.cloud>
2024-08-28 09:22:25 -0400

    Cambios en Collaterals

---
+++ DfcLoans/Autogenerated/Src/DfcSchedulePayment.DfcLoans.cs
@@ -0,0 +1,1 @@
+﻿/*Process is not valid. Fix invalid elements "Payment schedule successfull, Conditional flow 3, ConditionalSequenceFlow4, ConditionalSequenceFlow5, ConditionalSequenceFlow6" before starting it.*/
